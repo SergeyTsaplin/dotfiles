1629e2a211a82e2d9365b393d31e303e13076c0c
Sergei Tsaplin <s_tsaplin@wargaming.net>
2025-01-06 10:33:43 +0100

    Fish as a default shell

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,6 +4,8 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+config.default_prog = { '/usr/local/bin/fish', '-l' }
+
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
