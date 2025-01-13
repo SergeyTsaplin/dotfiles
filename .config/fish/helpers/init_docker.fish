32b012b097567c136f2e1b82d8cd68b43abdcca6
Codemageddon <github@codemageddon.me>
2025-01-13 14:44:28 +0100

    fix docker init

--- .config/fish/helpers/init_docker.fish
+++ .config/fish/helpers/init_docker.fish
@@ -1,7 +1,6 @@
 function init_docker
     if test -e $HOME/.rd/docker.sock
-        set --export DOCKER_HOST unix://$HOME/.rd/docker.sock
+        set -Ux DOCKER_HOST unix://$HOME/.rd/docker.sock
         fish_add_path -pmg "$HOME/.rd/bin"
     end
 end
-
