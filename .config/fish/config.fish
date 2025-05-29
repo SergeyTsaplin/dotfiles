5392323c516293c2f967a4f4dbabfcdfbf41d4cb
Codemageddon <github@codemageddon.me>
2025-05-29 03:06:41 +0200

    local changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,9 @@
 source $HOME/.config/fish/helpers/init_homebrew.fish
 source $HOME/.config/fish/helpers/init_docker.fish
+source $HOME/.config/fish/helpers/mise.fish
 init_homebrew
 init_docker
+init_mise
 
 if test -n "$WEZTERM_EXECUTABLE_DIR"
     fish_add_path -amg "$WEZTERM_EXECUTABLE_DIR"
