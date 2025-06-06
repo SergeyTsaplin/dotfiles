d67e834356a62bb82fedcc9208a75616765a1947
Sergei Tsaplin <s_tsaplin@wargaming.net>
2025-06-06 18:57:44 +0200

    remove cc from nvim

--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -3,5 +3,5 @@ vim.opt_local.shiftwidth = 4
 vim.opt_local.expandtab = true
 vim.opt_local.autoindent = true
 vim.opt_local.smartindent = true
-vim.opt_local.textwidth = 79
-vim.opt_local.colorcolumn = "80"
+vim.opt_local.textwidth = 88
+vim.opt_local.colorcolumn = nil
