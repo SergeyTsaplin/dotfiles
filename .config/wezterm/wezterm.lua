af5f9615860709903f20739cbe0d63b3a44a7667
Sergey Tsaplin <sergeytsaplin@gmail.com>
2025-06-08 22:41:19 +0200

    Hide wezterm window title

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,10 +5,10 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
 config.color_scheme = "Catppuccin Mocha (Gogh)"
 config.font = wezterm.font_with_fallback({ "FiraCode Nerd Font Mono", "Monaco" })
 config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
 
 config.initial_cols = 120
 config.initial_rows = 28
